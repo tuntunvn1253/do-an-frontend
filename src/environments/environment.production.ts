76f3856d34b1490fba02b05c1436410d60e8010e
Your Name <tuanlvps38702@gmail.com>
2026-03-25 19:07:02 +0700

    Update production API URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://du-an-cua-tuan.up.railway.app/api' // Thay bằng link Railway của bạn
+  apiUrl: 'http://do-an-backend-production.up.railway.app/api' // Thay bằng link Railway của bạn
 };
